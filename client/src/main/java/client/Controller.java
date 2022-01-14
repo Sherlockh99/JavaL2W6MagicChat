157590ffa254b566f72a797524b0c61d74adee1c
Sherlockh99 <sherlockh989@gmail.com>
2022-01-14 23:35:44 +0200

    Part 1. Client

---
+++ client/src/main/java/client/Controller.java
@@ -0,0 +1,20 @@
+package client;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    public TextField textField;
+    @FXML
+    public TextArea textArea;
+
+    @FXML
+    public void clickBtnSendText(ActionEvent actionEvent) {
+        textArea.appendText(textField.getText() + "\n");
+        textField.clear();
+        textField.requestFocus();
+    }
+}
