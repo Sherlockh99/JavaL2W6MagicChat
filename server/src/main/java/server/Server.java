fcba9b12c6d0289d52982320029d2cb7ef448240
Sherlockh99 <sherlockh989@gmail.com>
2022-01-14 23:39:35 +0200

    Part 2. Add Server

---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,50 @@
+package server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server {
+    private static ServerSocket server;
+    private static Socket socket;
+    private static final int PORT = 8189;
+    private static Scanner sc;
+    private static PrintWriter out;
+
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = server = new ServerSocket(PORT)) {
+            System.out.println("Server started!");
+            socket = server.accept();
+            System.out.println("Client connected!");
+
+            sc = new Scanner(socket.getInputStream());
+            out = new PrintWriter(socket.getOutputStream(),true); //сразу отправляет
+
+            while (true){
+                String str = sc.nextLine();
+                if (str.equals("/end")){
+                    break;
+                }
+                System.out.println("Client: " + str);
+                out.println("ECHO: " + str);
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }finally {
+            System.out.println("Client disconnect!");
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println();
+        };
+    }
+}
